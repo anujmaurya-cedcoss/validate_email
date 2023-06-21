ead647980d22887a2d8696bd3bc0f4e953f48cd1
Anuj Maurya <anujmaurya@cedcoss.com>
2023-06-21 13:11:19 +0530

    initial commit

---
+++ src/process.php
@@ -0,0 +1,7 @@
+<?php
+$email = $_POST['email'];
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "$email is a valid email address";
+} else {
+    echo "$email is not a valid email address";
+}
